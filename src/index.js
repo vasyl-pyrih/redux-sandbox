aa0162355e066bffe0c2fd0a814ecb0c9ffb4911
pyrigok <pyrigok@i.ua>
2019-08-14 16:19:56 +0300

    first redux app created.

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ const reducer = ( state = 0, action ) => {
     case 'INC':
       return state + 1;
 
+    case 'DEC':
+      return state - 1;
+
     default:
      return state;
   }
@@ -16,11 +19,24 @@ const reducer = ( state = 0, action ) => {
 
 const store = createStore(reducer);
 
-store.subscribe(() => {
-  console.log(store.getState());
-});
+document
+  .getElementById('inc')
+  .addEventListener('click', () => {
+    store.dispatch({type: 'INC'});
+  });
 
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
+document
+  .getElementById('dec')
+  .addEventListener('click', () => {
+    store.dispatch({type: 'DEC'});
+  });
 
-// N 104-107
+const update = () => {
+  document
+    .getElementById('counter')
+    .innerHTML = store.getState();
+};
+
+store.subscribe(update);
+
+// N 104-108
